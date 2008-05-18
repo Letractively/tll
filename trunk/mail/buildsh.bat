86b20c0d28b9d605763bf336213d50bbd0b72b97
jopaki <jopaki@00d174d0-0c4c-0410-b606-1bd1e6b23676>
2008-05-18 23:10:25 +0000

    Removed the need for buildsh.bat opting for setting necessary settings globally

--- trunk/mail/buildsh.bat
+++
@@ -1,9 +0,0 @@
-set JAVA_HOME=C:\Program Files\java\jdk1.6.0_05
-set ANT_HOME=C:\development\libs\apache-ant-1.7.0
-set COMMONS_COLLECTIONS_JAR=C:\development\libs\commons-collections-3.2.1\commons-collections-3.2.1.jar
-set COMMONS_CONFIGURATION_JAR=C:\development\libs\commons-configuration-1.5\commons-configuration-1.5.jar
-set COMMONS_LANG_JAR=C:\development\libs\commons-lang-2.3\commons-lang-2.3.jar
-set COMMONS_LOGGING_JAR=C:\development\libs\commons-logging-1.1.1\commons-logging-1.1.1.jar
-set TLL_CONFIG_JAR=C:\development\libs\tll\\config\dist\tll-config.jar
-set CLASSPATH=%ANT_HOME%\lib\ant.jar;.\config;%COMMONS_COLLECTIONS_JAR%;%COMMONS_CONFIGURATION_JAR%;%COMMONS_LANG_JAR%;%COMMONS_LOGGING_JAR%;%TLL_CONFIG_JAR%;
-%SystemRoot%\system32\cmd.exe
