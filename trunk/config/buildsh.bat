e7762192d7e6a065bd513e7db7acfa842381bd04
jopaki <jopaki@00d174d0-0c4c-0410-b606-1bd1e6b23676>
2008-05-18 00:41:20 +0000

    Moved the tll source root dir locally as part of some dir re-structuring for my local dev env.

--- trunk/config/buildsh.bat
+++ trunk/config/buildsh.bat
@@ -1,6 +1,6 @@
 set USERNAME=user
 set USERDOMAIN=domain
 set JAVA_HOME=C:\Program Files\java\jdk1.6.0_05
-set ANT_HOME=C:\development\Java\apache-ant-1.7.0
+set ANT_HOME=C:\development\libs\apache-ant-1.7.0
 set CLASSPATH=.;%ANT_HOME%\lib\ant.jar;
 %SystemRoot%\system32\cmd.exe
