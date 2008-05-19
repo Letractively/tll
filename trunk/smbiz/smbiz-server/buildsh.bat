047815c6693e8187c8f732803cd0c2dd7be2d034
jopaki <jopaki@00d174d0-0c4c-0410-b606-1bd1e6b23676>
2008-05-19 01:19:43 +0000

    Removed the not un-necessary buildsh.bat Removed the now un-needed lib dir since we are now referencing all needed libs in the new tools project. Adjusted some config properties based relying on the new TLL_ROOT classpath variable.

--- trunk/smbiz/smbiz-server/buildsh.bat
+++
@@ -1,5 +0,0 @@
-set JAVA_HOME=C:\Program Files\java\jdk1.6.0_05
-set ANT_HOME=C:\development\libs\apache-ant-1.7.0
-set ASPECTJ_HOME=C:\development\libs\aspectj-1.5.4
-set CLASSPATH=%ANT_HOME%\lib\ant.jar;%ASPECTJ_HOME%\lib\aspectjtools.jar;.\config;.\lib\commons-collections.jar;.\lib\commons-configuration.jar;.\lib\commons-lang.jar;.\lib\commons-logging.jar;.\lib\tll-config.jar;
-%SystemRoot%\system32\cmd.exe
