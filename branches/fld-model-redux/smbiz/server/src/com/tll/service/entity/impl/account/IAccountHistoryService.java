ed3f11b3b1817e99091652779bfe17d0a18bd146
jopaki <jopaki@00d174d0-0c4c-0410-b606-1bd1e6b23676>
2009-01-05 16:33:01 +0000

    Killing fld-model-redux branch

--- branches/fld-model-redux/smbiz/server/src/com/tll/service/entity/impl/account/IAccountHistoryService.java
+++
@@ -1,8 +0,0 @@
-package com.tll.service.entity.impl.account;
-
-import com.tll.model.impl.AccountHistory;
-import com.tll.service.entity.IEntityService;
-
-public interface IAccountHistoryService extends IEntityService<AccountHistory> {
-
-}
