c6693c92193d66144ac8463a03fc3c3ceaafa120
jopaki <jopaki@00d174d0-0c4c-0410-b606-1bd1e6b23676>
2009-01-18 21:34:56 +0000

    Stubbed the new tll-model module.

---
+++ branches/tll-mvn/model/src/main/java/com/tll/model/validate/PhoneNumberValidator.java
@@ -0,0 +1,46 @@
+/**
+ * The Logic Lab
+ * @author jpk Dec 22, 2007
+ */
+package com.tll.model.validate;
+
+import org.apache.oro.text.perl.Perl5Util;
+import org.hibernate.validator.Validator;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import com.tll.util.ValidationUtil;
+
+/**
+ * PhoneNumberValidator
+ * @author jpk
+ */
+public class PhoneNumberValidator implements Validator<PhoneNumber>, IPropertyReference {
+
+	private String phonePropertyName;
+	private String countryPropertyName;
+
+	public String getPropertyReference() {
+		return phonePropertyName;
+	}
+
+	public void initialize(PhoneNumber parameters) {
+		phonePropertyName = parameters.phonePropertyName();
+		countryPropertyName = parameters.countryPropertyName();
+	}
+
+	public boolean isValid(Object value) {
+		if(value == null) return true;
+		BeanWrapper bw = new BeanWrapperImpl(value);
+		Object pvPhone = bw.getPropertyValue(phonePropertyName);
+		Object pvCountry = bw.getPropertyValue(countryPropertyName);
+		if(pvPhone == null) return true;
+
+		final String phoneNumber = ((String) pvPhone).trim().toLowerCase();
+		final String country = pvCountry == null ? "us" : ((String) pvCountry).trim().toLowerCase();
+
+		return (new Perl5Util()).match(ValidationUtil.isValidUsaStateAbbr(country) ? ValidationUtil.US_PHONE_REGEXP
+				: ValidationUtil.INTNL_PHONE_REGEXP, phoneNumber);
+	}
+
+}
---
+++ branches/tll-mvn/model/src/main/java/com/tll/model/validate/SSN.java
@@ -0,0 +1,27 @@
+/**
+ * The Logic Lab
+ * @author jpk
+ * Dec 22, 2007
+ */
+package com.tll.model.validate;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.hibernate.validator.ValidatorClass;
+
+/**
+ * SSN - Indicates the annotatee is a social security number.
+ * @author jpk
+ */
+@ValidatorClass(SSNValidator.class)
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SSN {
+
+	String message() default "{validator.ssn}";
+}
