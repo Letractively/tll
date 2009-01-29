6bea45b695bc6b3069ed920ff56d92449ff57a6d
jopaki <jopaki@00d174d0-0c4c-0410-b606-1bd1e6b23676>
2009-01-29 20:01:03 +0000

    Removing unused branch.

--- branches/tll-mvn-csm/smbiz/src/main/java/com/tll/service/entity/account/IAccountHistoryService.java
+++
@@ -1,8 +0,0 @@
-package com.tll.service.entity.account;
-
-import com.tll.model.AccountHistory;
-import com.tll.service.entity.IEntityService;
-
-public interface IAccountHistoryService extends IEntityService<AccountHistory> {
-
-}
