26bce5d7bcf591b800e8e86ea4428983fd2a7a66
jopaki <jopaki@00d174d0-0c4c-0410-b606-1bd1e6b23676>
2009-02-07 14:45:12 +0000

    kill tll-mvn branch.

--- branches/tll-mvn/smbiz/server/src/main/java/com/tll/service/entity/account/IAccountService.java
+++
@@ -1,9 +0,0 @@
-package com.tll.service.entity.account;
-
-import com.tll.model.Account;
-import com.tll.service.entity.INamedEntityService;
-import com.tll.service.entity.IStatefulEntityService;
-
-public interface IAccountService extends INamedEntityService<Account>, IStatefulEntityService<Account> {
-
-}
