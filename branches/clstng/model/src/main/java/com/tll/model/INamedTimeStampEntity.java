1f72c922e60ae7198e28dd2511cfd89898719487
jopaki <jopaki@00d174d0-0c4c-0410-b606-1bd1e6b23676>
2009-03-20 18:56:37 +0000

    Killing clstng dev branch.

--- branches/clstng/model/src/main/java/com/tll/model/INamedTimeStampEntity.java
+++
@@ -1,8 +0,0 @@
-package com.tll.model;
-
-/**
- * Interface for entities that have time stamps and names.
- * @author jpk
- */
-public interface INamedTimeStampEntity extends ITimeStampEntity, INamedEntity {
-}
