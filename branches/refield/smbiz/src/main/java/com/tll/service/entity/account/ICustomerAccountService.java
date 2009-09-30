2e674e27e8f38698d173ea3c5ae964e0ab97589e
jopaki <jopaki@00d174d0-0c4c-0410-b606-1bd1e6b23676>
2009-09-30 13:57:14 +0000

    Killing refield branch.

--- branches/refield/smbiz/src/main/java/com/tll/service/entity/account/ICustomerAccountService.java
+++
@@ -1,8 +0,0 @@
-package com.tll.service.entity.account;
-
-import com.tll.model.CustomerAccount;
-import com.tll.service.entity.IStatefulEntityService;
-
-public interface ICustomerAccountService extends IStatefulEntityService<CustomerAccount> {
-
-}
