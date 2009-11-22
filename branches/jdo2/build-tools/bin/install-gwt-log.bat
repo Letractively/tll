93ccab68c437431c15b31ca69effe92c9ebd6156
jopaki <jopaki@00d174d0-0c4c-0410-b606-1bd1e6b23676>
2009-11-22 21:48:27 +0000

    -Updated GWT dependency to 2.0.0-rc1 -Updated gwt-log dependency to 3.0.0 -Fixed mvn install batch files in build-tools

--- branches/jdo2/build-tools/bin/install-gwt-log.bat
+++ branches/jdo2/build-tools/bin/install-gwt-log.bat
@@ -2,9 +2,9 @@ rem ****************************************************
 rem Installs gwt-log to local maven repo.
 rem ****************************************************
 
-set /P LOC="gwt-log-2.6.2.jar location (specify the parent dir)?"
-set VERSION=2.6.2
+set /P LOC="gwt-log jar location? "
+set /P VERSION="gwt-log version? "
 
-call mvn install:install-file -Dfile=%LOC%\gwt-log-2.6.2.jar -DgroupId=com.allen_sauer.gwt.log -DartifactId=gwt-log -Dversion=%VERSION% -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=%LOC%\gwt-log-%VERSION%.jar -DgroupId=com.allen_sauer.gwt.log -DartifactId=gwt-log -Dversion=%VERSION% -Dpackaging=jar -DgeneratePom=true
 
 pause
--- branches/jdo2/build-tools/bin/install-gwt.bat
+++ branches/jdo2/build-tools/bin/install-gwt.bat
@@ -3,17 +3,13 @@ rem Installs GWT into the local maven repo from GWT source root.
 rem NOTE: GWT project is assumed to be built.
 rem ***********************************************************
 
-set /P GWT_HOME="GWT source root dir?"
-set /P GWTV="GWT version?"
+set /P GWT_INSTALL_DIR="GWT install dir? "
+set /P GWTV="GWT version? "
 
-call mvn install:install-file -Dfile=%GWT_HOME%\build\lib\ant-gwt.jar -DgroupId=com.google.gwt -DartifactId=ant-gwt -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=%GWT_HOME%\build\lib\gwt-api-checker.jar -DgroupId=com.google.gwt -DartifactId=gwt-api-checker -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=%GWT_HOME%\build\lib\gwt-benchmark-viewer.war -DgroupId=com.google.gwt -DartifactId=gwt-benchmark-viewer -Dversion=%GWTV% -Dpackaging=war -DgeneratePom=true
-call mvn install:install-file -Dfile=%GWT_HOME%\build\lib\gwt-customchecks.war -DgroupId=com.google.gwt -DartifactId=gwt-customchecks -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=%GWT_HOME%\build\lib\gwt-dev.jar -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=%GWT_HOME%\build\lib\gwt-doctool.jar -DgroupId=com.google.gwt -DartifactId=gwt-doctool -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=%GWT_HOME%\build\lib\gwt-servlet.jar -DgroupId=com.google.gwt -DartifactId=gwt-servlet -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=%GWT_HOME%\build\lib\gwt-soyc-vis.jar -DgroupId=com.google.gwt -DartifactId=gwt-soyc-vis -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=%GWT_HOME%\build\lib\gwt-user.jar -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=%GWT_INSTALL_DIR%\gwt-api-checker.jar -DgroupId=com.google.gwt -DartifactId=gwt-api-checker -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=%GWT_INSTALL_DIR%\gwt-dev.jar -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=%GWT_INSTALL_DIR%\gwt-servlet.jar -DgroupId=com.google.gwt -DartifactId=gwt-servlet -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=%GWT_INSTALL_DIR%\gwt-soyc-vis.jar -DgroupId=com.google.gwt -DartifactId=gwt-soyc-vis -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=%GWT_INSTALL_DIR%\gwt-user.jar -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=%GWTV% -Dpackaging=jar -DgeneratePom=true
 
 pause
